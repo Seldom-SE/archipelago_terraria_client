a3fdd41e717411dbb1a6e2e66d908cbbdef2fe96
Seldom <HaydenBadgerAZ@gmail.com>
2023-01-17 17:16:28 -0700

    Partially working achievements

--- SeldomArchipelago.cs
+++ SeldomArchipelago.cs
@@ -2,15 +2,20 @@ using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
 using SeldomArchipelago.Systems;
+using System.Reflection;
 using Terraria;
 using Terraria.GameContent.Events;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Social;
 
 namespace SeldomArchipelago
 {
     public class SeldomArchipelago : Mod
     {
+        // TODO
+        // Multiplayer doesn't look like it connected
+
         // Terraria is single-threaded so this *should* be fine
         bool temp;
 
@@ -126,6 +131,100 @@ namespace SeldomArchipelago
                 });
                 cursor.Index++;
                 cursor.EmitDelegate<Action>(() => Main.hardMode = temp);
+            };
+
+            On.Terraria.Achievements.AchievementCondition.Complete += (orig, condition) =>
+            {
+                var mode = typeof(SocialAPI).GetField("_mode", BindingFlags.Static | BindingFlags.NonPublic);
+                mode.SetValue(null, SocialMode.None);
+                orig(condition);
+                mode.SetValue(null, SocialMode.Steam);
+            };
+
+            Main.Achievements.OnAchievementCompleted += achievement =>
+            {
+                var name = achievement.Name switch
+                {
+                    "TIMBER" => "Timber!!",
+                    "NO_HOBO" => "No Hobo",
+                    "OBTAIN_HAMMER" => "Stop! Hammer Time!",
+                    "OOO_SHINY" => "Ooo! Shiny!",
+                    "HEART_BREAKER" => "Heart Breaker",
+                    "HEAVY_METAL" => "Heavy Metal",
+                    "I_AM_LOOT" => "I Am Loot!",
+                    "STAR_POWER" => "Star Power",
+                    "HOLD_ON_TIGHT" => "Hold on Tight!",
+                    "SMASHING_POPPET" => "Smashing, Poppet!",
+                    "WHERES_MY_HONEY" => "Where's My Honey?",
+                    "DUNGEON_HEIST" => "Dungeon Heist",
+                    "ITS_GETTING_HOT_IN_HERE" => "It's Getting Hot in Here",
+                    "MINER_FOR_FIRE" => "Miner for Fire",
+                    "LIKE_A_BOSS" => "Like a Boss",
+                    "BLOODBATH" => "Bloodbath",
+                    "NOT_THE_BEES" => "Not the Bees!",
+                    "JEEPERS_CREEPERS" => "Jeepers Creepers",
+                    "FUNKYTOWN" => "Funkytown",
+                    "INTO_ORBIT" => "Into Orbit",
+                    "ROCK_BOTTOM" => "Rock Bottom",
+                    "FASHION_STATEMENT" => "Fashion Statement",
+                    "VEHICULAR_MANSLAUGHTER" => "Vehicular Manslaughter",
+                    "LUCKY_BREAK" => "Lucky Break",
+                    "THROWING_LINES" => "Throwing Lines",
+                    "DYE_HARD" => "Dye Hard",
+                    "THE_CAVALRY" => "The Cavalry",
+                    "COMPLETELY_AWESOME" => "Completely Awesome",
+                    "TIL_DEATH" => "Til Death...",
+                    "WATCH_YOUR_STEP" => "Watch Your Step!",
+                    "YOU_CAN_DO_IT" => "You Can Do It!",
+                    "MATCHING_ATTIRE" => "Matching Attire",
+                    "BENCHED" => "Benched",
+                    "FOUND_GRAVEYARD" => "Quiet Neighborhood",
+                    "GO_LAVA_FISHING" => "Hot Reels!",
+                    "TALK_TO_NPC_AT_MAX_HAPPINESS" => "Leading Landlord",
+                    "PET_THE_PET" => "Feeling Petty",
+                    "DIE_TO_DEAD_MANS_CHEST" => "Dead Men Tell No Tales",
+                    "STICKY_SITUATION" => "Sticky Situation",
+                    "THERE_ARE_SOME_WHO_CALL_HIM" => "There are Some Who Call Him...",
+                    "DECEIVER_OF_FOOLS" => "Deceiver of Fools",
+                    "ARCHAEOLOGIST" => "Archaeologist",
+                    "PRETTY_IN_PINK" => "Pretty in Pink",
+                    "GET_TERRASPARK_BOOTS" => "Boots of the Hero",
+                    "FLY_A_KITE_ON_A_WINDY_DAY" => "A Rather Blustery Day",
+                    "TURN_GNOME_TO_STATUE" => "Heliophobia",
+                    "THROW_A_PARTY" => "Jolly Jamboree",
+                    "GLORIOUS_GOLDEN_POLE" => "Glorious Golden Pole",
+                    "SERVANT_IN_TRAINING" => "Servant-in-Training",
+                    "GOOD_LITTLE_SLAVE" => "10 Fishing Quests",
+                    "TROUT_MONKEY" => "Trout Monkey",
+                    "FAST_AND_FISHIOUS" => "Fast and Fishious",
+                    "SUPREME_HELPER_MINION" => "Supreme Helper Minion!",
+                    "HEAD_IN_THE_CLOUDS" => "Head in the Clouds",
+                    "BEGONE_EVIL" => "Begone, Evil!",
+                    "EXTRA_SHINY" => "Extra Shiny!",
+                    "DRAX_ATTAX" => "Drax Attax",
+                    "PHOTOSYNTHESIS" => "Photosynthesis",
+                    "GET_A_LIFE" => "Get a Life",
+                    "KILL_THE_SUN" => "Kill the Sun",
+                    "MECHA_MAYHEM" => "Mecha Mayhem",
+                    "PRISMANCER" => "Prismancer",
+                    "IT_CAN_TALK" => "It Can Talk?!",
+                    "GELATIN_WORLD_TOUR" => "Gelatin World Tour",
+                    "TOPPED_OFF" => "Topped Off",
+                    "DEFEAT_DREADNAUTILUS" => "Don't Dread on Me",
+                    "TEMPLE_RAIDER" => "Temple Raider",
+                    "ROBBING_THE_GRAVE" => "Robbing the Grave",
+                    "BALEFUL_HARVEST" => "Baleful Harvest",
+                    "ICE_SCREAM" => "Ice Scream",
+                    "SWORD_OF_THE_HERO" => "Sword of the Hero",
+                    "BIG_BOOTY" => "Big Booty",
+                    "REAL_ESTATE_AGENT" => "Real Estate Agent",
+                    "RAINBOWS_AND_UNICORNS" => "Rainbows and Unicorns",
+                    "SICK_THROW" => "Sick Throw",
+                    "YOU_AND_WHAT_ARMY" => "You and What Army?",
+                    _ => null,
+                };
+
+                if (name != null) ArchipelagoSystem.QueueLocation(name);
             };
         }
     }
--- Systems/ArchipelagoSystem.cs
+++ Systems/ArchipelagoSystem.cs
@@ -127,6 +127,79 @@ namespace SeldomArchipelago.Systems
                     case "Post-Lunatic Cultist": NPC.downedAncientCultist = true; break;
                     case "Post-Lunar Events": NPC.downedTowerNebula = NPC.downedTowerSolar = NPC.downedTowerStardust = NPC.downedTowerVortex = true; break;
                     case "Post-Moon Lord": NPC.downedMoonlord = true; break;
+                    case "Hermes Boots": GiveItem(ItemID.HermesBoots); break;
+                    case "Magic Mirror": GiveItem(ItemID.MagicMirror); break;
+                    case "Cloud in a Bottle": GiveItem(ItemID.CloudinaBalloon); break;
+                    case "Grappling Hook": GiveItem(ItemID.GrapplingHook); break;
+                    case "Climbing Claws": GiveItem(ItemID.ClimbingClaws); break;
+                    case "Fledgling Wings": GiveItem(ItemID.CreativeWings); break;
+                    case "Demon Conch": GiveItem(ItemID.DemonConch); break;
+                    case "Magic Conch": GiveItem(ItemID.MagicConch); break;
+                    case "Anklet of the Wind": GiveItem(ItemID.AnkletoftheWind); break;
+                    case "Aglet": GiveItem(ItemID.Aglet); break;
+                    case "Ice Skates": GiveItem(ItemID.IceSkates); break;
+                    case "Lava Charm": GiveItem(ItemID.LavaCharm); break;
+                    case "Obsidian Rose": GiveItem(ItemID.ObsidianRose); break;
+                    case "Nature's Gift": GiveItem(ItemID.NaturesGift); break;
+                    case "Feral Claws": GiveItem(ItemID.FeralClaws); break;
+                    case "Magma Stone": GiveItem(ItemID.MagmaStone); break;
+                    case "Shark Tooth Necklace": GiveItem(ItemID.SharkToothNecklace); break;
+                    case "Cobalt Shield": GiveItem(ItemID.CobaltShield); break;
+                    case "Band of Regeneration": GiveItem(ItemID.BandofRegeneration); break;
+                    case "Philosopher's Stone": GiveItem(ItemID.PhilosophersStone); break;
+                    case "Cross Necklace": GiveItem(ItemID.CrossNecklace); break;
+                    case "Magic Quiver": GiveItem(ItemID.MagicQuiver); break;
+                    case "Rifle Scope": GiveItem(ItemID.RifleScope); break;
+                    case "Celestial Magnet": GiveItem(ItemID.CelestialMagnet); break;
+                    case "Rod of Discord": GiveItem(ItemID.RodofDiscord); break;
+                    case "Flying Carpet": GiveItem(ItemID.FlyingCarpet); break;
+                    case "Lifeform Analyzer": GiveItem(ItemID.LifeformAnalyzer); break;
+                    case "Ancient Chisel": GiveItem(ItemID.AncientChisel); break;
+                    case "Moon Charm": GiveItem(ItemID.MoonCharm); break;
+                    case "Neptune's Shell": GiveItem(ItemID.NeptunesShell); break;
+                    case "Shoe Spikes": GiveItem(ItemID.ShoeSpikes); break;
+                    case "Tabi": GiveItem(ItemID.Tabi); break;
+                    case "Black Belt": GiveItem(ItemID.BlackBelt); break;
+                    case "Flesh Knuckles": GiveItem(ItemID.FleshKnuckles); break;
+                    case "Putrid Scent": GiveItem(ItemID.PutridScent); break;
+                    case "Paladin's Shield": GiveItem(ItemID.PaladinsShield); break;
+                    case "Frozen Turtle Shell": GiveItem(ItemID.FrozenTurtleShell); break;
+                    case "Star Cloak": GiveItem(ItemID.StarCloak); break;
+                    case "Discount Card": GiveItem(ItemID.DiscountCard); break;
+                    case "Red Counterweight": GiveItem(ItemID.RedCounterweight); break;
+                    case "Yoyo Glove": GiveItem(ItemID.YoYoGlove); break;
+                    case "Depth Meter": GiveItem(ItemID.DepthMeter); break;
+                    case "Compass": GiveItem(ItemID.Compass); break;
+                    case "Radar": GiveItem(ItemID.Radar); break;
+                    case "DPS Meter": GiveItem(ItemID.DPSMeter); break;
+                    case "Metal Detector": GiveItem(ItemID.MetalDetector); break;
+                    case "Sextant": GiveItem(ItemID.Sextant); break;
+                    case "Stopwatch": GiveItem(ItemID.Stopwatch); break;
+                    case "Tally Counter": GiveItem(ItemID.TallyCounter); break;
+                    case "Fisherman's Pocket Guide": GiveItem(ItemID.FishermansGuide); break;
+                    case "High Test Fishing Line": GiveItem(ItemID.HighTestFishingLine); break;
+                    case "Angler Earring": GiveItem(ItemID.AnglerEarring); break;
+                    case "Tackle Box": GiveItem(ItemID.TackleBox); break;
+                    case "Lavaproof Fishing Hook": GiveItem(ItemID.LavaFishingHook); break;
+                    case "Weather Radio": GiveItem(ItemID.WeatherRadio); break;
+                    case "Blindfold": GiveItem(ItemID.Blindfold); break;
+                    case "Pocket Mirror": GiveItem(ItemID.PocketMirror); break;
+                    case "Vitamins": GiveItem(ItemID.Vitamins); break;
+                    case "Armor Polish": GiveItem(ItemID.ArmorPolish); break;
+                    case "Adhesive Bandage": GiveItem(ItemID.AdhesiveBandage); break;
+                    case "Bezoar": GiveItem(ItemID.Bezoar); break;
+                    case "Nazar": GiveItem(ItemID.Nazar); break;
+                    case "Megaphone": GiveItem(ItemID.Megaphone); break;
+                    case "Trifold Map": GiveItem(ItemID.TrifoldMap); break;
+                    case "Fast Clock": GiveItem(ItemID.FastClock); break;
+                    case "Brick Layer": GiveItem(ItemID.BrickLayer); break;
+                    case "Extendo Grip": GiveItem(ItemID.ExtendoGrip); break;
+                    case "Paint Sprayer": GiveItem(ItemID.PaintSprayer); break;
+                    case "Portable Cement Mixer": GiveItem(ItemID.PortableCementMixer); break;
+                    case "Treasure Magnet": GiveItem(ItemID.TreasureMagnet); break;
+                    case "Step Stool": GiveItem(ItemID.PortableStool); break;
+                    case "Gold Ring": GiveItem(ItemID.GoldRing); break;
+                    case "Lucky Coin": GiveItem(ItemID.LuckyCoin); break;
                 }
 
                 Chat($"Recieved {itemName} from {session.Players.GetPlayerAlias(item.Player)}!");
@@ -188,6 +261,8 @@ namespace SeldomArchipelago.Systems
 
         public static void QueueLocation(string locationName)
         {
+            if (session == null) return;
+
             var location = session.Locations.GetLocationIdFromName("Terraria", locationName);
             session.Locations.CompleteLocationChecks(new long[] { location });
             locationQueue.Add(session.Locations.ScoutLocationsAsync(new long[] { location }));
