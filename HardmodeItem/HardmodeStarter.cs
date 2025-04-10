7ad1597b437f849da8d46700342ac41ffd7d8e05
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2025-04-10 18:26:15 -0400

    Added HardmodeStarter

---
+++ HardmodeItem/HardmodeStarter.cs
@@ -0,0 +1,31 @@
+﻿using SeldomArchipelago.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static SeldomArchipelago.Systems.ArchipelagoSystem;
+
+namespace SeldomArchipelago.HardmodeItem
+{
+    public class HardmodeStarter : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.DemonHeart);
+        }
+
+        public override bool CanUseItem(Player player) => !Main.hardMode;
+
+        public override bool? UseItem(Player player)
+        {
+            ActivateHardmode();
+            return true;
+        }
+    }
+}
